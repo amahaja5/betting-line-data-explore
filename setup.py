5449970f087506297fbbf570612e340471fbe4ed
Avi Mahajan <mahajan.avi@gmail.com>
2021-01-17 17:03:45 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name='betting_lines',
+    version='1.0.0',
+    url='https://github.com/amahaja5/betting-line-data-explore',
+    license='MIT',
+    author='amahaja5',
+    description='python implementation of The Odds API',
+    packages=find_packages()
+)
